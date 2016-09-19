14f227d273db17380908bba6b6ebb061376649fb
oop <oop@pc01>
2016-09-19 07:37:32 -0500

    Crear clase calculadora

---
+++ src/usingevents/Calculator.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package usingevents;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author oop
+ */
+public class Calculator extends JFrame 
+{
+    private JLabel display;
+    private JButton operator[];
+    private JButton numbers[];   
+    private JButton clear;
+    
+    //--------------------------------------------------------------------------
+    
+    public Calculator()
+    {
+              
+    }
+    
+    //--------------------------------------------------------------------------
+    
+    private void widgets()
+    {
+        
+    }
+    
+    //--------------------------------------------------------------------------
+    
+    private void events()
+    {
+        
+    }
+    
+    //--------------------------------------------------------------------------
+    
+    public static void main(String[] args) 
+    {
+        new Calculator();
+    }
+}
