39af41a1f1c6fd6b3007e978cb9b35ddbbdf22f9
oop <oop@pc01>
2016-09-26 07:40:38 -0500

    BlackJack

---
+++ src/blackjack/BlanckJack.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blackjack;
+
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author oop
+ */
+public class BlanckJack 
+{
+    private MyButton b[];
+    private JLabel d;
+    //--------------------------------------------------------------------------
+    
+    public BlanckJack()
+    {
+        
+    }
+    //--------------------------------------------------------------------------
+    
+    private void widgets()
+    {
+        
+    }
+    //--------------------------------------------------------------------------
+
+    
+    private void events()
+    {
+        
+    }
+    //--------------------------------------------------------------------------
+
+    
+    public static void main(String[] args) 
+    {
+        
+    }    
+   
+
+    
+    
+}
---
+++ src/blackjack/MyButton.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blackjack;
+
+/**
+ *
+ * @author oop
+ */
+public class MyButton 
+{
+    private int v;
+    
+    public MyButton(int v)
+    {
+        
+    }
+    
+    public void show ()
+    {
+        
+    }
+    
+}
